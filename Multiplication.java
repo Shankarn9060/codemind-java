1f45d60b368c3ccc47bfc6375f1f1fd88a3b140d
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2022-09-09 10:51:19 +0530

    import java.util.Scanner; class Airthmetic { 	public static void main(String args[]) 	{ 		int a,b; 		Scanner sc=new Scanner(System.in); 		a=sc.nextInt(); 		b=sc.nextInt();
    		System.out.println(a*b);
    		
    		sc.close();
    	}
    
    }

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Airthmetic
+{
+	public static void main(String args[])
+	{
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+
+		System.out.println(a*b);
+		
+		sc.close();
+	}
+
+}
