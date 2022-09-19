c8fc11f42a59ce79b591e4fe349dc82e7fdfe20a
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2022-09-19 20:16:53 +0530

    import java.util.Scanner; import java.lang.Math; class pirme {     public static void main(String args[])     {         int v,k,r,l=0,n;         Scanner sc=new Scanner(System.in);         r=sc.nextInt();         v=sc.nextInt();         for(k=r;k<=v;k++)         {             n=0;             if(k==1)             {                 continue;             }             for(int j=2;j<=Math.sqrt(k);j++)             {                 if(k%j==0)                 {                     n++;                     break;                 }             }             if(n==0)             {                 l++;             }         }         System.out.println(l);         sc.close();     } }

---
+++ Primes_in_range.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.lang.Math;
+class pirme
+{
+    public static void main(String args[])
+    {
+        int v,k,r,l=0,n;
+        Scanner sc=new Scanner(System.in);
+        r=sc.nextInt();
+        v=sc.nextInt();
+        for(k=r;k<=v;k++)
+        {
+            n=0;
+            if(k==1)
+            {
+                continue;
+            }
+            for(int j=2;j<=Math.sqrt(k);j++)
+            {
+                if(k%j==0)
+                {
+                    n++;
+                    break;
+                }
+            }
+            if(n==0)
+            {
+                l++;
+            }
+        }
+        System.out.println(l);
+        sc.close();
+    }
+}
