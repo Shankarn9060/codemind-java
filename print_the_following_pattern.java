ce475ea68d7db4b71c436d4f2e99977273bb7eb8
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2022-09-23 21:04:05 +0530

    import java.util.*; class Pattern { 	public static void main(String[] args) 	{ 	    int r; 	    Scanner sc=new Scanner(System.in); 	    r=sc.nextInt(); 		for(int i=0;i<r;i++) 		{ 		    for(int j=r;j>=1;j--) 		    { 		        System.out.print(j+" "); 		    } 		    System.out.println(); 		} 	} }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,16 +1,16 @@
 import java.util.*;
-class pat
+class Pattern
 {
 	public static void main(String[] args)
 	{
 	    int r;
 	    Scanner sc=new Scanner(System.in);
 	    r=sc.nextInt();
-		for(int i=r;i>=1;i--)
+		for(int i=0;i<r;i++)
 		{
-		    for(int j=1;j<=i;j++)
+		    for(int j=r;j>=1;j--)
 		    {
-		        System.out.print(j);
+		        System.out.print(j+" ");
 		    }
 		    System.out.println();
 		}
