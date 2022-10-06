cf2d962343ad764277ce74c2cdd268c60a27d873
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2022-10-06 19:31:49 +0530

    import java.util.*; class pnv{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int k=0;         long v=sc.nextLong();         while(v>0){             v=v/10;             k++;             if(k==10 && v==0){                 k--;             }         }         if(k==9){             System.out.println("Valid");         }         else{             System.out.println("Invalid");         }     } }

--- PHONE_NUMBER_VALIDATION:.java
+++ PHONE_NUMBER_VALIDATION:.java
@@ -1,26 +1,20 @@
 import java.util.*;
-class Phone
-{
-    public static void main(String[] args)
-    {
+class pnv{
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int k=0;
         long v=sc.nextLong();
-        while(v>0)
-        {
+        while(v>0){
             v=v/10;
             k++;
-            if(k==10 && v==0)
-            {
+            if(k==10 && v==0){
                 k--;
             }
         }
-        if(k==9)
-        {
+        if(k==9){
             System.out.println("Valid");
         }
-        else
-        {
+        else{
             System.out.println("Invalid");
         }
     }
