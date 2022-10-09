0ff7f94443a6dd939d8ab0a2c0d4627d31b72dab
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2022-10-09 12:36:23 +0530

    import java.util.*; class Diagonals_of_Polygon {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         System.out.println(n*(n-3)/2);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Diagonals_of_Polygon
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n*(n-3)/2);
+    }
+}
