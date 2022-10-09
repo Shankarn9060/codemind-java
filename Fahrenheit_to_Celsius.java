1da7a0cd08ed7d3d9d86c4473d5fe52b7679e489
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2022-10-09 15:19:19 +0530

    import java.util.*; class Celsius {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         float n=sc.nextInt();         double a=(double)(n-32)/1.8;         System.out.format("%.2f",a);     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Celsius
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        float n=sc.nextInt();
+        double a=(double)(n-32)/1.8;
+        System.out.format("%.2f",a);
+    }
+}
