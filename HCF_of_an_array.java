a50faef0c04f91564826cbdc68f4e044d26eaca4
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2022-10-09 12:42:16 +0530

    import java.util.*; class HCF {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int hcf=arr[0],j=1;         while(j<n)         {             if(arr[j]%hcf==0)             {                 j++;             }             else             {                 hcf=arr[j]%hcf;             }         }         System.out.println(hcf);     } }

---
+++ HCF_of_an_array.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class HCF
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int hcf=arr[0],j=1;
+        while(j<n)
+        {
+            if(arr[j]%hcf==0)
+            {
+                j++;
+            }
+            else
+            {
+                hcf=arr[j]%hcf;
+            }
+        }
+        System.out.println(hcf);
+    }
+}
