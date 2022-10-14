25de2b7dc221de3fcbb460c602b5e09788dc31e8
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2022-10-14 19:47:08 +0530

    import java.util.Scanner; class sol { 	public static void main(String[] args) 	{ 		Scanner sc=new Scanner(System.in); 		String a=sc.nextLine(); 		System.out.println("Hello Technicalhub"); 		System.out.println(a); 	} }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class sol
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		System.out.println("Hello Technicalhub");
+		System.out.println(a);
+	}
+}
