013c97b42c6791a0295d29385f184bccae129555
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2022-11-27 18:45:47 +0530

    import java.util.Scanner; class Reverse_string {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in); 		String s=sc.nextLine();         String[] vk=s.split(" ");         for (int i=vk.length-1;i>=0;i--)         {             System.out.print(vk[i]+" ");         }     } }

--- Reverse_String.java
+++ Reverse_String.java
@@ -1,14 +1,14 @@
-import java.util.*;
-class ReverseString
+import java.util.Scanner;
+class Reverse_string
 {
     public static void main(String args[])
     {
-        String initial, rev="";
-        Scanner in=new Scanner(System.in);
-        initial=in.nextLine();
-        int length=initial.length();
-        for(int i=length-1;i>=0;i--)
-            rev=rev+initial.charAt(i);
-        System.out.println(rev);
+        Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+        String[] vk=s.split(" ");
+        for (int i=vk.length-1;i>=0;i--)
+        {
+            System.out.print(vk[i]+" ");
+        }
     }
 }
